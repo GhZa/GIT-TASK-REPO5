a73414ee2d97f625ca91a7618f01c82990ae1aa0
GhZa <gheorghe.zabulica1@gmail.com>
2020-10-28 15:09:57 +0200

    update main5

--- main5.java
+++ main5.java
@@ -1,7 +1,7 @@
 class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-	System.out.println("Bye");
-	System.out.println("Bye");
+	    System.out.println("Bye world");
+	    System.out.println("Bye world");
     }
 }
