29d5e8efe06497e6feca911812c35732344a1b73
GhZa <gheorghe.zabulica1@gmail.com>
2020-10-30 00:09:43 +0200

    commit1

--- main5.java
+++ main5.java
@@ -1,7 +1,20 @@
 class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-	    System.out.println("Bye world");
-	    System.out.println("Bye world");
+        System.out.println("Bye world");
+        System.out.println("Bye world");
+        reNr(228);
+        printSMH("cikkk");
+
+    }
+
+    public static void printSMH(String a){
+        System.out.println(a);
+    }
+
+    public static int reNr(int x){
+        x = x+12;
+        return x;
+
     }
 }
