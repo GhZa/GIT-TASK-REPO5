2f1445f72dfce955c82dfc061722400c02b64715
GhZa <gheorghe.zabulica1@gmail.com>
2020-10-29 17:05:14 +0200

    yes

--- main5.java
+++ main5.java
@@ -1,7 +1,11 @@
 class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+<<<<<<< HEAD
 	    System.out.println("Bye world");
 	    System.out.println("Bye world");
+=======
+	System.out.println("Bye World..");
+>>>>>>> 4c7f074 (changing main5 class)
     }
 }
